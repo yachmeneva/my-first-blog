d4d9058247776abbcd3eaf5ce686781eb0fda864
Olga Yachmeneva <admin@admin.com>
2016-03-03 17:55:00 +0300

    Final changes up to 22 ch. upd

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+__author__ = 'user'
+
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text')
